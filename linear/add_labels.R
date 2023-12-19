1c7d1d6eb7b823fa89b7bf5164b2a29b33acba30
Dennis Chiuten <dennis@terrascope.com>
2023-12-19 17:00:24 +0800

    script and query to add labels to Linear issues

---
+++ linear/add_labels.R
@@ -0,0 +1,61 @@
+
+# purpose -----------------------------------------------------------------
+
+# assign Old Pod labels to issues created from Jira import
+# Starting Stuff ----------------------------------------------------------
+pacman::p_load(
+  tidyverse,
+  lubridate,
+  scales,
+  zoo,
+  patchwork,
+  keyring,
+  DBI,
+  RPostgreSQL,
+  httr,
+  RJSONIO
+)
+pacman::p_load_current_gh("denchiuten/tsViz")
+theme_set(theme_ts())
+
+query <- read_file("linear_issue_query.sql")
+
+# pull data from Redshift ---------------------------------------------------------------
+con <- aws_connect()
+df_raw <- dbFetch(dbSendQuery(con, query))
+
+
+# get list of label values ------------------------------------------------
+
+issueQuery <- "
+  {
+    issueLabels {
+      nodes {
+        id
+        name
+      }
+    }
+  }
+"
+url <- "https://api.linear.app/graphql"
+
+label_response <- POST(
+  url,
+  body = toJSON(list(query = query)),
+  add_headers(
+    Authorization = key_get("linear"),
+    "Content-Type" = "application/json"
+    )
+  )
+parsed_response <- content(label_response, as = "text") |> 
+  fromJSON(flatten = T)
+
+# convert to data frame 
+df_labels <- bind_rows(parsed_response$data$issueLabels)
+
+# review query output -----------------------------------------------------
+
+# check to see if any linear issues are linked to multiple Jira issues
+df_grouped <- df_raw |> 
+  count(linear_issue_id) |> 
+  filter(n > 1)
---
+++ linear/linear_issue_query.sql
@@ -0,0 +1,17 @@
+SELECT
+	lissue.id AS linear_issue_id,
+	team.name AS team_name,
+	team.id AS team_id,
+	jissue.key AS jira__issue_key,
+	jissue.id AS jira_issue_id,
+	p.key AS jira_project_key
+FROM linear.issue AS lissue
+INNER JOIN linear.team AS team 
+	ON lissue.team_id = team.id
+INNER JOIN linear.attachment AS att 
+	ON lissue.id = att.issue_id
+	AND att.url IS NOT NULL
+INNER JOIN jra.issue AS jissue 
+	ON att.url = 'https://gpventure.atlassian.net/browse/' || jissue.key
+INNER JOIN jra.project AS p
+	ON jissue.project = p.id
