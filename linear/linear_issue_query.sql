15f1fbe2473364d39fafd98be7991b96bc487915
Dennis Chiuten <dennis@terrascope.com>
2023-12-20 09:40:04 +0800

    added bunch of filters for deleted rows from fivetran

--- linear/linear_issue_query.sql
+++ linear/linear_issue_query.sql
@@ -9,10 +9,17 @@ SELECT
 FROM linear.issue AS lissue
 INNER JOIN linear.team AS team 
 	ON lissue.team_id = team.id
+	AND team._fivetran_deleted IS FALSE
 INNER JOIN linear.attachment AS att 
 	ON lissue.id = att.issue_id
 	AND att.url IS NOT NULL
+	AND att._fivetran_deleted IS FALSE
 INNER JOIN jra.issue AS jissue 
 	ON att.url = 'https://gpventure.atlassian.net/browse/' || jissue.key
+	AND jissue._fivetran_deleted IS FALSE
 INNER JOIN jra.project AS p
 	ON jissue.project = p.id
+	AND jissue._fivetran_deleted IS FALSE
+WHERE
+	1 = 1
+	AND lissue._fivetran_deleted IS FALSE
