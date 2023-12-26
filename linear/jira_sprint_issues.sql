0b3d5ed41e964174febf815d3910b57ac1d10128
Dennis Chiuten <dennis@terrascope.com>
2023-12-26 18:36:55 +0800

    whatever other queries I forgot to commit

--- linear/jira_sprint_issues.sql
+++ linear/jira_sprint_issues.sql
@@ -7,6 +7,6 @@ INNER JOIN jra.issue_board AS ib
 	ON i.id = ib.issue_id
 INNER JOIN jra.sprint AS s
 	ON ib.board_id = s.board_id
-	AND s.state = 'future'
-	AND s.start_date >= '2024-01-10'
+	AND s.state <> 'current'
+	AND s.start_date >= '2023-12-20'
 ORDER BY 2
