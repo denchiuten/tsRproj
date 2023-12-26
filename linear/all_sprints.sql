c75e22731bf5805ea2dd5c965f6f0b2ab15c4c6a
Dennis Chiuten <dennis@terrascope.com>
2023-12-26 18:07:50 +0800

    exclude CUSIMP sprints

--- linear/all_sprints.sql
+++ linear/all_sprints.sql
@@ -2,10 +2,9 @@ SELECT
 	s.id AS sprint_id,
 	s.state,
 	s.name,
-	s.start_date,
-	s.end_date,
-	b.name,
-	b.type
+	s.start_date::DATE AS start_date,
+	s.end_date::DATE AS end_date,
+	b.name
 FROM jra.sprint AS s
 INNER JOIN jra.board AS b
 	ON s.board_id = b.id
@@ -13,5 +12,6 @@ INNER JOIN jra.board AS b
 WHERE
 	1 = 1
 	AND s._fivetran_deleted IS FALSE
-	AND s.start_date >= '2024-01-01'
+	AND s.start_date >= '2023-12-20'
 	AND s.state <> 'closed'
+	AND b.name <> 'CUSIMP board'
