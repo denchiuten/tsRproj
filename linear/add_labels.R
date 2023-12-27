6232825014be19f795c6b9c4f8a72d68bb0d61eb
Dennis Chiuten <dennis@terrascope.com>
2023-12-27 08:54:12 +0800

    change to GraphQL query instead of Redshift query to find Linear issues

--- linear/add_labels.R
+++ linear/add_labels.R
@@ -56,17 +56,136 @@ df_labels <- bind_rows(parsed_response$data$issueLabels)
 
 
 
-# get issues without the label --------------------------------------------
-issue_query <- "
-  {
-    issueLabels(
-      filter: { 
-        name: { contains: \"Pod\" }
-        }
-      ) {
-      nodes {id, name}
+# pull all Linear issues without JIRA Project Label---------------------------------------------
+
+fetch_issues <- function(url, cursor = NULL) {
+  if(is.null(cursor)) {
+    query <- str_glue(
+      "{{
+        issues(filter: {{ labels: {{parent: {{name: {{neq: \"JIRA Project\"}}}}}} }}, first: 100) {{
+          pageInfo {{
+            endCursor
+            hasNextPage
+          }} 
+          nodes {{
+            id 
+            identifier
+            state {{
+              name
+            }}
+            attachments {{
+              nodes {{
+                id
+                url
+              }}
+            }}
+          }}
+        }}
+      }}"
+    )
+  } else {
+    query <- str_glue(
+      "
+      {{
+        issues(filter: {{ cycle: {{null: true}} }}, first: 100, after: \"{cursor}\") {{
+          pageInfo {{
+            endCursor
+            hasNextPage
+          }} 
+          nodes {{
+            id 
+            identifier
+             state {{
+              name
+            }}
+            attachments {{
+              nodes {{
+                id
+                url
+              }}
+            }}
+          }}
+        }}
+      }}
+    "
+    )
+  }
+  
+  response <- POST(
+    url, 
+    body = toJSON(list(query = query)), 
+    add_headers(
+      Authorization = key_get("linear"),
+      "Content-Type" = "application/json"
+    )
+  )
+  return(fromJSON(content(response, as = "text"), flatten = TRUE))
+}
+
+
+# run a loop to fetch issues with pagination--------------------------------------------------------------
+
+# initialize variables for pagination
+all_issues <- list()
+has_next_page <- TRUE
+cursor <- NULL
+
+# loop
+while(has_next_page == TRUE) {
+  response_data <- fetch_issues(api_url, cursor)
+  all_issues <- c(all_issues, response_data$data$issues$nodes)
+  cursor <- response_data$data$issues$pageInfo$endCursor
+  has_next_page <- response_data$data$issues$pageInfo$hasNextPage
+}
+
+
+# Flatten the data and create a data frame, thanks ChatGPT ----------------
+
+df_linear_issues <- map_df(
+  all_issues, 
+  ~ {
+    issue_id <- .x[["id"]]
+    issue_identifier <- .x[["identifier"]]
+    
+    # Initialize an empty data frame for attachments
+    attachments_df <- data.frame(
+      attachment_id = character(),
+      attachment_url = character(),
+      stringsAsFactors = FALSE
+    )
+    
+    # Check if attachments are present and correctly structured
+    if (!is.null(.x[["attachments"]]) && "nodes" %in% names(.x[["attachments"]])) {
+      if (length(.x[["attachments"]][["nodes"]]) > 0) {
+        attachments_df <- map_df(
+          .x[["attachments"]][["nodes"]], 
+          ~ data.frame(
+            attachment_id = .x[["id"]],
+            attachment_url = .x[["url"]],
+            stringsAsFactors = FALSE
+          )
+        )
       }
-  }"
+    }
+    
+    # If there are no attachments, create a single row with NAs
+    if (nrow(attachments_df) == 0) {
+      attachments_df <- data.frame(
+        attachment_id = NA, 
+        attachment_url = NA, 
+        stringsAsFactors = FALSE
+      )
+    }
+    
+    # Combine issue information with attachments
+    data.frame(
+      issue_id, 
+      issue_identifier, 
+      attachments_df
+    )
+  }, 
+  .id = NULL
+)
 
 # review query output -----------------------------------------------------
 
