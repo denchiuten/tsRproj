14c0d553071a6260f9a97c6be0684b7830f1cece
Dennis Chiuten <dennis@terrascope.com>
2024-01-03 17:31:37 +0800

    working script to create projects in Linear and write mapping table of Notion to Linear project IDs to a GSheet for import into Redshift

--- notion/migrate_projects.R
+++ notion/migrate_projects.R
@@ -21,28 +21,48 @@ pacman::p_load(
 notion_query <- read_file("pull_notion_projects.sql")
 user_query <- read_file("user_lookup.sql")
 source("json_functions.R")
-
-api_url <- "https://api.linear.app/graphql"
+gsheet_url <- "https://docs.google.com/spreadsheets/d/14zGbsRF_86pGHOE3qIG9OJRXrSfRpJJV2OJWM4TwgQk/edit#gid=0"
+gs4_auth("dennis@terrascope.com")
 # pull raw data -----------------------------------------------------------
 con <- aws_connect()
 df_notion_raw <- dbFetch(dbSendQuery(con, notion_query))
 df_user_lookup <- dbFetch(dbSendQuery(con, user_query))
-
+df_project_lookup <- dbFetch(dbSendQuery(con, "SELECT * FROM plumbing.notion_project_to_linear_project"))
 # clean up ----------------------------------------------------------------
 df_notion_clean <- df_notion_raw |> 
+  # parse the stupid json columns
   mutate(
+    linear_project_id = NA,
     driver_id = sapply(driver_json, parse_id),
     approver_id = sapply(approver_json, parse_id),
     contributors_id = sapply(contributors_json, parse_id),
     informed_id = sapply(informed_json, parse_id),
     status = sapply(project_status_json, parse_id),
-    title = sapply(project_title_json, parse_title)
+    title = sapply(project_title_json, parse_title),
+  ) |> 
+  # now do some processing on the output columns
+  mutate(
+    # replace driver with the creator when driver is missing
+    across(driver_id, ~ ifelse(is.na(.), creator_id, .)),
+    # adapt title for character limit in Linear project name
+    across(title, ~substr(., 1, 80))
   ) |> 
   select(!contains("json")) |> 
-  filter(!status %in% c("canceled", "done")) |> 
   left_join(
     df_user_lookup,
     by = c("driver_id" = "notion_user_id")
+    ) |> 
+  rename(driver_linear_id = linear_user_id) |> 
+  filter(
+    !status %in% c("canceled", "done"),
+    title != "New Project",
+    !is.na(driver_linear_id)
+    ) |> 
+  arrange(title) |> 
+  # now filter out any that already have a project created
+  anti_join(
+    df_project_lookup,
+    by = c("project_page_id" = "notion_project_page_id")
     )
 
 # Linear API function to create project -----------------------------------
@@ -55,7 +75,7 @@ create_project <- function(project_name, lead_id) {
         input: {{
           teamIds: \"87fa2ccb-12f4-4a4c-8d7f-37db1d23e8e4\"
           name: \"{project_name}\" 
-          response$data$projectCreate$project
+          leadId: \"{lead_id}\"
         }}
         ) {{
           success
@@ -107,7 +127,7 @@ add_project_link <- function(project_id, url, link_label) {
 for (i in 1:nrow(df_notion_clean)) {
   
   project_name <- df_notion_clean$title[i]
-  lead_id <- df_notion_clean$driver_id[i]
+  lead_id <- df_notion_clean$driver_linear_id[i]
   url <- df_notion_clean$page_url[i]
 
   # first create the project
@@ -118,16 +138,30 @@ for (i in 1:nrow(df_notion_clean)) {
     
     # now add the link to the Notion page
     project_id <- create_response$data$projectCreate$project
+    df_notion_clean$linear_project_id[i] <- project_id
     link_response <- add_project_link(project_id, url, "Notion Project")
     if (!is.null(link_response$data)) {
       # success
       print(str_glue("Added {url} to {project_name}"))
     } else {
       # what went wrong
-      print(str_glue("Failed to add link to {project_name}: \"{link_response$errors[[1]]$message}\""))
+      print(str_glue("Failed to add link to {project_name}: \"{link_response$errors[[1]]$extensions$userPresentableMessage}\""))
     }
   } else {
     # what went wrong
-    print(str_glue("Failed to create {project_name}: \"{create_response$errors[[1]]$message}\" ({i} of {nrow(df_notion_clean)})"))
+    print(str_glue("Failed to create {project_name}: \"{create_response$errors[[1]]$extensions$userPresentableMessage}\" ({i} of {nrow(df_notion_clean)})"))
   }
 }
+
+
+# write output to gsheet --------------------------------------------------
+
+df_output <- df_notion_clean |> 
+  select(
+    notion_project_page_id = project_page_id,
+    linear_project_id
+  ) 
+
+ss <- gs4_get(gsheet_url)  
+write_sheet(df_output, ss, sheet = str_glue("df_output_{now()}"))
+
