8a5cda46cf2c736ef0e262d826fd5a35abef0d4e
Dennis Chiuten <dennis@terrascope.com>
2024-01-03 14:06:43 +0800

    typo fix for Approver

--- linear/pull_notion_projects.sql
+++ linear/pull_notion_projects.sql
@@ -9,7 +9,7 @@ WITH daci AS (
 			AND dop.database_object_id = '7fe0dfb4-f84f-4b3b-8fc3-cca27a621022'
 		WHERE 
 			1 = 1
-			AND dop.name IN ('Driver', 'Contributors', 'Informed', 'Approvers')
+			AND dop.name IN ('Driver', 'Contributors', 'Informed', 'Approver')
 			AND dop._fivetran_deleted IS FALSE
 			AND pp._fivetran_deleted IS FALSE
 )
