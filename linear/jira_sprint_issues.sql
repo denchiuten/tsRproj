ac38deeca580b37a7d7996cfa83425310318cc0d
Dennis Chiuten <dennis@terrascope.com>
2024-01-19 15:17:23 +0800

    corrected mapping of issue ID to sprint ID via customfield_10020

--- linear/jira_sprint_issues.sql
+++ linear/jira_sprint_issues.sql
@@ -10,11 +10,15 @@ FROM jra.issue AS i
 INNER JOIN jra.project AS p
 	ON i.project = p.id
 	AND p.key NOT IN ('CUSIMP', 'DBCLEAN')
-INNER JOIN jra.issue_board AS ib
-	ON i.id = ib.issue_id
+INNER JOIN jra.vw_latest_issue_multiselect_value AS f
+	ON i.id = f.issue_id
+	AND f.field_id = 'customfield_10020' -- custom field_id for sprint
 INNER JOIN jra.sprint AS s
-	ON ib.board_id = s.board_id
+	ON f.value = s.id
 	AND s.state <> 'closed'
 	AND s.start_date >= '2023-12-20'
 INNER JOIN plumbing.jira_sprint_to_linear_cycle AS map
 	ON s.id = map.jira_sprint_id
+WHERE
+	1 = 1
+	AND i._fivetran_deleted IS FALSE
