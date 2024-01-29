d56514a1b49c9c81c30e95b8d5ce16cd10800304
Dennis Chiuten <dennis@terrascope.com>
2024-01-29 14:13:46 +0800

    script to convert exported financial reporting into long format for import into Redshift

--- finance_reporting/budget_wrangle.R
+++ finance_reporting/budget_wrangle.R
@@ -22,13 +22,22 @@ df_raw <- read_xlsx("fy24_budget_for_tableau_test.xlsx")
 df_clean <- df_raw |> 
   filter(if_any(everything(), ~ !is.na(.)))
 
-x <- df_clean |> 
-  select(where(~ !is.na(as.numeric(.))))
-
 df_long <- df_clean |> 
   pivot_longer(
     cols = starts_with("4"),
     names_to = "date"
     ) |> 
   mutate(across(date, ~ as.Date(as.numeric(.), origin = "1899-12-30"))) |> 
-  filter(value != 0)
+  filter(value != 0) |> 
+  rename(
+    olam_view = `Olam View`,
+    mv_cost_centre = `MV Cost center`,
+    finance_cost_centre = `Finance Cost Centre`,
+    team = Team,
+    country = Country,
+    pnl = `P&L`,
+    cash_commitment_binary = CC,
+    mgmt_pnl_cost_type =`Cost type - Management P&L`,
+    general_ledger_desc = `G/L Description`,
+    description = Description
+  )
