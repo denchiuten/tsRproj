29cdbc8c8d545f697e10ffe4f25b4b990d25f92b
Dennis Chiuten <dennis@terrascope.com>
2024-01-30 13:27:17 +0800

    minor query edits

--- linear/auth0_label_query.sql
+++ linear/auth0_label_query.sql
@@ -1,4 +1,7 @@
-SELECT name || ' - ' || id AS label_name
+SELECT 
+	name AS organization_name,
+	id AS organization_id,
+	name || ' - ' || id AS label_name
 FROM auth0.organization
 WHERE _fivetran_deleted IS FALSE
 ORDER BY 1
