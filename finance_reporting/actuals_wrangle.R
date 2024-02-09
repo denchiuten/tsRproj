70cb41f7ff577ad0c21ee1eaf1d5e21da3c8b4f7
Dennis Chiuten <dennis@terrascope.com>
2024-02-09 11:21:40 +0800

    a bunch of data wrangling

---
+++ finance_reporting/actuals_wrangle.R
@@ -0,0 +1,65 @@
+# Starting Stuff ----------------------------------------------------------
+pacman::p_load(
+  tidyverse,
+  lubridate,
+  scales,
+  zoo,
+  patchwork,
+  keyring,
+  DBI,
+  RPostgreSQL,
+  readxl,
+  googlesheets4
+)
+
+gs4_auth("dennis@terrascope.com")
+actuals_url <- "https://docs.google.com/spreadsheets/d/1lUw1IX8xnQ6pBbCvbXOY7ZhXcTbnjdcA4JfCo26xmLM/edit#gid=0"
+ss <- gs4_get(actuals_url)
+close_date <- as.Date("2024-01-31")
+# read in file and clean it -----------------------------------------------
+
+df_raw <- read_sheet(ss)
+# filter out empty rows
+df_clean <- df_raw |> 
+  filter(
+    if_any(everything(), ~ !is.na(.)),
+    !is.na(`P&L`),
+    !is.na(CC)
+  )
+
+df_long <- df_clean |> 
+  pivot_longer(
+    cols = matches("^[0-9]"),
+    names_to = "date"
+  ) |> 
+  filter(value != 0) |> 
+  rename(
+    lt_ppt_mapping = `LT PPT Mapping`,
+    location = Location,
+    olam_view = `Olam View`,
+    mv_cost_centre = `MV Cost center`,
+    finance_cost_centre = `Finance Cost Centre`,
+    team = Team,
+    country = Country,
+    pnl = `P&L`,
+    cash_commitment = CC,
+    mgmt_pnl_cost_type =`Cost type - Management P&L`,
+    general_ledger_desc = `G/L Description`,
+    description = Description
+  ) |> 
+  mutate(
+    across(date, ~ dmy(.)),
+    close_date = close_date,
+    across(
+      c(pnl, cash_commitment), 
+      ~ifelse(. == 1, TRUE, FALSE)
+    ),
+    across(finance_cost_centre, as.numeric),
+    across(finance_cost_centre, ~replace_na(., 0))
+  )
+
+
+# # write and import --------------------------------------------------------
+
+write_csv(df_long, file = str_glue("actuals_{today()}.csv"))
+
--- finance_reporting/budget_wrangle.R
+++ finance_reporting/budget_wrangle.R
@@ -8,43 +8,57 @@ pacman::p_load(
   keyring,
   DBI,
   RPostgreSQL,
-  readxl
+  readxl,
+  googlesheets4
 )
-pacman::p_load_current_gh("denchiuten/tsViz")
-theme_set(theme_ts())
 
+gs4_auth("dennis@terrascope.com")
 
 # read in file and clean it -----------------------------------------------
 
-df_raw <- read_xlsx("fy24_budget_for_tableau_test.xlsx")
+df_raw <- read_xlsx("Terrascope FY24 Budget_Final.xlsx")
 
 # filter out empty rows
 df_clean <- df_raw |> 
-  filter(if_any(everything(), ~ !is.na(.)))
+  filter(
+    if_any(everything(), ~ !is.na(.)),
+    !is.na(`P&L`),
+    !is.na(CC)
+    )
 
 df_long <- df_clean |> 
   pivot_longer(
     cols = starts_with("4"),
     names_to = "date"
     ) |> 
-  mutate(
-    across(date, ~ as.Date(as.numeric(.), origin = "1899-12-30")),
-    import_date = today()
-    ) |> 
   filter(value != 0) |> 
   rename(
+    lt_ppt_mapping = `LT PPT Mapping`,
+    location = Location,
     olam_view = `Olam View`,
     mv_cost_centre = `MV Cost center`,
     finance_cost_centre = `Finance Cost Centre`,
     team = Team,
     country = Country,
     pnl = `P&L`,
-    cash_commitment_binary = CC,
+    cash_commitment = CC,
     mgmt_pnl_cost_type =`Cost type - Management P&L`,
     general_ledger_desc = `G/L Description`,
     description = Description
+  ) |> 
+  mutate(
+    location = "",
+    across(date, ~ as.Date(as.numeric(.), origin = "1899-12-30")),
+    import_date = today(),
+    across(
+      c(pnl, cash_commitment), 
+      ~ifelse(. == 1, TRUE, FALSE)
+    ),
+    across(finance_cost_centre, as.numeric),
+    across(finance_cost_centre, ~replace_na(., 0))
   )
 
-# write and import --------------------------------------------------------
+# # write and import --------------------------------------------------------
 
-write_csv(df_long, file = str_glue("{today()}.csv"))
+write_csv(df_long, file = str_glue("budget_{today()}.csv"))
+
