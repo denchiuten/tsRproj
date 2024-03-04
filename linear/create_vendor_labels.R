553c044c040a9252e17fbca5abde672251205e3f
Dennis Chiuten <dennis@terrascope.com>
2024-03-04 15:11:09 +0800

    revised query to make parent label ID dynamic based on alphabetical sorting of child label name

--- linear/create_vendor_labels.R
+++ linear/create_vendor_labels.R
@@ -21,7 +21,6 @@ pacman::p_load(
 
 gs4_auth("dennis@terrascope.com")
 gsheet_url <- "https://docs.google.com/spreadsheets/d/1BQQmRd_nkGhgKD1fF_N-hrOWSW6HtDVlS-DHadTvAYo/edit#gid=1820078108"
-parent_id <- "768db20f-6ff3-43f9-accb-579be6e93e35" #label_id for the label group, Vendor Name - Singapore
 
 source("linear_functions.R")
 # pull and prep data from GSheet ------------------------------------------
@@ -30,15 +29,25 @@ ss <- gs4_get(gsheet_url)
 df_raw <- read_sheet(ss)
 
 df_final <- df_raw |> 
-  mutate(label_name = str_glue("{`Entity Name`} - {`Vendor Code`}"))
+  mutate(label_name = str_glue("{`Entity Name`} - {`Vendor Code`}")) |> 
+  arrange(label_name) |> 
+  # assign parent label IDs based on letter range of child label name
+  mutate(
+    parent_id = case_when(
+      tolower(substr(label_name, 1, 1)) >= "a" & tolower(substr(label_name, 1, 1)) <= "c" ~ "768db20f-6ff3-43f9-accb-579be6e93e35",
+      tolower(substr(label_name, 1, 1)) >= "d" & tolower(substr(label_name, 1, 1)) <= "k" ~ "fe764f7b-3b39-49ba-b453-9a4926630772",
+      tolower(substr(label_name, 1, 1)) >= "l" & tolower(substr(label_name, 1, 1)) <= "r" ~ "5c895c36-1888-42b0-bc5c-8992cec62f75",
+      tolower(substr(label_name, 1, 1)) >= "s" & tolower(substr(label_name, 1, 1)) <= "z" ~ "d1b6b2ea-2aec-4d84-9e44-7e41acf721f7"
+    )
+  )
 
 
 # run a loop to create the labels -----------------------------------------
 
-for (i in 2:nrow(df_final)) {
+for (i in 1:nrow(df_final)) {
   
   label_name <- df_final$label_name[i]
-  
+  parent_id <- df_final$parent_id[i]
   response <- create_label(label_name, parent_id)
   # Check response
   if (!is.null(response$data)) {
