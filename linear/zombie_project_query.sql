a637e4356b02b8469d4c3d645eecb92566f8e519
Dennis Chiuten <dennis@terrascope.com>
2024-03-21 18:37:15 +0800

    query mods

--- linear/zombie_project_query.sql
+++ linear/zombie_project_query.sql
@@ -1,13 +1,20 @@
 SELECT
 	p.id AS project_id,
 	p.name AS project_name,
+	p.created_at::DATE AS created_date,
+	p.updated_at::DATE AS updated_date,
 	lead.name AS lead_name,
 	lead.id AS lead_id,
+	creator.name AS creator_name,
+	creator.id AS creator_id,
 	ws.type AS issue_status,
-	COUNT(DISTINCT i.id) AS n_issues
+	COUNT(i.id) AS n_issues
 FROM linear.project AS p
 LEFT JOIN linear.users AS lead
 	ON p.lead_id = lead.id
+LEFT JOIN linear.users AS creator
+	ON p.creator_id = creator.id
+	AND creator._fivetran_deleted IS FALSE
 LEFT JOIN linear.roadmap_to_project AS rp
 	ON p.id = rp.project_id
 	-- roadmap IDs for 2024 Features and Implementation Roadmaps, so we can filter out projects on these roadmaps
@@ -34,4 +41,4 @@ WHERE
 	-- exclude any that belong to the roadmap
 	AND rp.project_id IS NULL
 	AND t.id IS NULL
-GROUP BY 1,2,3,4,5
+GROUP BY 1,2,3,4,5,6,7,8,9
