af810ed5d9dff5a634e71ab3163eb16f1ef971fd
Dennis Chiuten <dennis@terrascope.com>
2024-04-02 09:38:38 +0800

    WITH statement to do multiple JOINs to exclude cases where the creator and assignee teams are the same

--- linear/issue_creator_team_query.sql
+++ linear/issue_creator_team_query.sql
@@ -1,8 +1,21 @@
+WITH employee_team AS (
+	SELECT 
+		u.id AS user_id,
+		u.email,
+		et.team_id,
+		et.team_name
+	FROM linear.users AS u
+	INNER JOIN bob.employee AS e
+		ON LOWER(u.email) = LOWER(e.email)
+	INNER JOIN bob.vw_employee_team AS et
+		ON e.id = et.employee_id
+)
+
 SELECT
 	i.id AS issue_id,
 	i.identifier,
-	u.email AS creator_email,
-	et.team_name,
+	creator.email AS creator_email,
+	creator.team_name,
 	l2.id AS label_id
 FROM linear.issue AS i
 INNER JOIN linear.team AS t
@@ -21,14 +34,13 @@ LEFT JOIN (
 		AND il._fivetran_deleted IS FALSE
 ) AS request_team
 	ON i.id = request_team.issue_id
-INNER JOIN linear.users AS u
-	ON i.creator_id = u.id
-INNER JOIN bob.employee AS e
-	ON lower(u.email) = LOWER(e.email)
-INNER JOIN bob.vw_employee_team AS et
-	ON e.id = et.employee_id
+INNER JOIN employee_team AS creator
+	ON i.creator_id = creator.user_id
+INNER JOIN employee_team AS assignee
+	ON i.assignee_id = assignee.user_id
+	AND creator.team_id <> assignee.team_id
 LEFT JOIN linear.label AS l2
-	ON et.team_name || ' Team' = l2.name
+	ON creator.team_name || ' Team' = l2.name
 	AND l2.parent_id = '591c8377-11b9-43ab-9b39-58b3f7e9a36b'
 WHERE
 	1 = 1
